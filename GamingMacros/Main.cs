455fbb95e5a8165a40e442753620b406a31d1b86
BenjaminDagg <bdagg@csumb.edu>
2018-12-01 16:18:52 -0800

    added automatic mouse movement to get past AFK timers

--- GamingMacros/Main.cs
+++ GamingMacros/Main.cs
@@ -15,6 +15,7 @@ using System.Management;
 using WindowsInput.Native;
 using WindowsInput;
 using System.Data.SQLite;
+using System.Runtime.InteropServices;
 
 
 namespace GamingMacros
@@ -94,6 +95,61 @@ namespace GamingMacros
         [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
         private static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
 
+        [DllImport("user32.dll")]
+        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+#pragma warning disable 649
+        internal struct INPUT
+        {
+            public UInt32 Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+        internal struct MOUSEINPUT
+        {
+            public Int32 X;
+            public Int32 Y;
+            public UInt32 MouseData;
+            public UInt32 Flags;
+            public UInt32 Time;
+            public IntPtr ExtraInfo;
+        }
+
+        private void ClickOnPoint(IntPtr wndHandle, Point clientPoint)
+        {
+            var oldPos = Cursor.Position;
+
+            /// get screen coordinates
+            ClientToScreen(wndHandle, ref clientPoint);
+
+            /// set cursor on coords, and press mouse
+            Cursor.Position = new Point(clientPoint.X, clientPoint.Y);
+
+            var inputMouseDown = new INPUT();
+            inputMouseDown.Type = 0; /// input type mouse
+            inputMouseDown.Data.Mouse.Flags = 0x0002; /// left button down
+
+            var inputMouseUp = new INPUT();
+            inputMouseUp.Type = 0; /// input type mouse
+            inputMouseUp.Data.Mouse.Flags = 0x0004; /// left button up
+
+            var inputs = new INPUT[] { inputMouseDown, inputMouseUp };
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            /// return mouse 
+            Cursor.Position = oldPos;
+        }
+
 
         private void populateProgramBox()
         {
@@ -159,6 +215,7 @@ namespace GamingMacros
         {
             const int KEY_INTERVAL = 200; //time to hold each key
             const int KEY_REPS = 3; //number of times to press each key
+            const int PAUSE_INTERVAL = 2000;
 
             //w a s d keys
             VirtualKeyCode[] keys = {VirtualKeyCode.VK_W,
@@ -178,11 +235,12 @@ namespace GamingMacros
                 {
                     return;
                 }
+                
+                
+                
+                
 
-                //melee
-                this.sendInput.Keyboard.KeyDown(VirtualKeyCode.VK_C);
-                Thread.Sleep(KEY_INTERVAL);
-                this.sendInput.Keyboard.KeyUp(VirtualKeyCode.VK_C);
+                this.sendInput.Mouse.MoveMouseBy(1000,0);
 
                 //press current keys index for KEY_REPS time
                 for (int i = 0; i < KEY_REPS && this.macroRunning == true; i++)
@@ -196,6 +254,7 @@ namespace GamingMacros
                     this.sendInput.Keyboard.KeyDown(keys[counter]);
                     Thread.Sleep(KEY_INTERVAL);
                     this.sendInput.Keyboard.KeyUp(keys[counter]);
+                    
 
 
                 }
@@ -203,11 +262,11 @@ namespace GamingMacros
                 //go to next element or return back to first
                 counter = (counter + 1) % (keys.Length);
 
-
+                Thread.Sleep(PAUSE_INTERVAL);
 
             }
 
-
+            
 
 
         }
@@ -306,7 +365,7 @@ namespace GamingMacros
         private void MouseWasClicked(object sender, EventArgs e)
         {
             Process p = this.GetForegroundProcessName();
-            this.targetProcess = p;
+            //this.targetProcess = p;
 
             
 
@@ -342,16 +401,7 @@ namespace GamingMacros
                 Console.WriteLine("not null " + selectedProcess.ProcessName);
                 this.targetProcess = selectedProcess;
 
-                //shift focus to selected program
-                //ShowWindow(this.targetProcess.Handle, ShowWindowEnum.ShowNormal);
-                if (this.targetProcess.MainWindowHandle == IntPtr.Zero)
-                {
-                    // the window is hidden so try to restore it before setting focus.
-                    ShowWindow(this.targetProcess.Handle, ShowWindowEnum.Restore);
-                }
-
-                // set user the focus to the window
-                SetForegroundWindow(this.targetProcess.MainWindowHandle);
+                
             }
             
 
@@ -400,6 +450,8 @@ namespace GamingMacros
         }
     }
 }
+
+
 
 public static class MouseHook
 {
@@ -816,6 +868,61 @@ public static class DataAccess
 
         return entries;
     }
+
+
+}
+
+public class Win32
+{
+    // The WM_COMMAND message is sent when the user selects a command item from 
+    // a menu, when a control sends a notification message to its parent window, 
+    // or when an accelerator keystroke is translated.
+    public const int WM_KEYDOWN = 0x100;
+    public const int WM_KEYUP = 0x101;
+    public const int WM_COMMAND = 0x111;
+    public const int WM_LBUTTONDOWN = 0x201;
+    public const int WM_LBUTTONUP = 0x202;
+    public const int WM_LBUTTONDBLCLK = 0x203;
+    public const int WM_RBUTTONDOWN = 0x204;
+    public const int WM_RBUTTONUP = 0x205;
+    public const int WM_RBUTTONDBLCLK = 0x206;
+
+    // The FindWindow function retrieves a handle to the top-level window whose
+    // class name and window name match the specified strings.
+    // This function does not search child windows.
+    // This function does not perform a case-sensitive search.
+    [DllImport("User32.dll")]
+    public static extern int FindWindow(string strClassName, string strWindowName);
+
+    // The FindWindowEx function retrieves a handle to a window whose class name 
+    // and window name match the specified strings.
+    // The function searches child windows, beginning with the one following the
+    // specified child window.
+    // This function does not perform a case-sensitive search.
+    [DllImport("User32.dll")]
+    public static extern int FindWindowEx(
+        int hwndParent,
+        int hwndChildAfter,
+        string strClassName,
+        string strWindowName);
+
+
+    // The SendMessage function sends the specified message to a window or windows. 
+    // It calls the window procedure for the specified window and does not return
+    // until the window procedure has processed the message. 
+    [DllImport("User32.dll")]
+    public static extern Int32 SendMessage(
+        int hWnd,               // handle to destination window
+        int Msg,                // message
+        int wParam,             // first message parameter
+        [MarshalAs(UnmanagedType.LPStr)] string lParam); // second message parameter
+
+    [DllImport("User32.dll")]
+    public static extern Int32 SendMessage(
+        int hWnd,               // handle to destination window
+        int Msg,                // message
+        int wParam,             // first message parameter
+        int lParam);            // second message parameter
 }
 
 
